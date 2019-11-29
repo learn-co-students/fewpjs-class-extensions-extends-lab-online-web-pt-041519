d817d7d83a9f27b1ffff36a8f8f0e215a856dbc3
github username <github email address>
2019-11-29 08:28:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Your code here
+// Polygon class
+
+class Polygon {
+   // accepts array of integers in constructor
+   constructor(sidesArray) {
+      this.sidesArray = sidesArray
+   }
+
+   get countSides() {
+      let count = 0
+      for(let i = 0; i < this.sidesArray.length; i++) {
+         count += 1
+      }
+      return count
+   }
+
+   get perimeter() {
+      let totalOfSides = this.sidesArray.reduce((acc, curr) => acc += curr)
+      return totalOfSides
+   }
+}
+
+// triangle has to inherit from Polygon
+class Triangle extends Polygon {
+
+   get isValid () {
+      if ((this.sidesArray[0] + this.sidesArray[1] <= this.sidesArray[2]) || (this.sidesArray[0]+this.sidesArray[2] <= this.sidesArray[1]) || (this.sidesArray[1] + this.sidesArray[2] <= this.sidesArray[0])) {
+        return false
+      } 
+      else {
+        return true
+      }
+   }
+}
+
+
+class Square extends Polygon {
+
+   get isValid() {
+      return this.sidesArray.every((el) => el === this.sidesArray[0])
+   }
+
+   get area() {
+      return this.sidesArray.reduce((acc, curr) => acc += curr)
+   }
+}
